c47831a2377caa97f41eaba8900f5d3e556a8087
saad <mounaime.saad@gmail.com>
2023-06-30 19:52:20 +0100

    Initial commit

---
+++ SpringAngular/src/main/java/com/example/SpringAngular/CustomerRepo/CustomerRepo.java
@@ -0,0 +1,11 @@
+package com.example.SpringAngular.CustomerRepo;
+
+import com.example.SpringAngular.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
+
+@EnableJpaRepositories
+@Repository
+public interface CustomerRepo extends JpaRepository<Customer,Integer> {
+}
